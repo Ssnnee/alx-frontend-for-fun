8b03fd090f4772081587da91bef54a29aa8cc307
Samuel Nandi <nandisamuelsne@gmail.com>
2024-07-21 23:59:49 +0100

    refactor: evoding mutiple writing

--- markdown2html.py
+++ markdown2html.py
@@ -8,7 +8,6 @@ from pathlib import Path
 
 def markdown2html():
     """Main function of the script."""
-    arr_to_write = []
 
     if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html",
@@ -21,17 +20,16 @@ def markdown2html():
             sys.exit(1)
 
     with open(sys.argv[1], 'r') as mdfile:
-        content = mdfile.readlines()
+        lines = mdfile.readlines()
 
-    for line in content:
-        parsed_line = line.split()
-        if (parsed_line[0].startswith("#")):
-            level = parsed_line[0].count("#")
-            line_content = line[level:-1].strip()
-            with open(sys.argv[2], mode="w", encoding="utf-8") as file:
-                arr_to_write.append("<h{}>{}</h{}>\n".format(
-                    level, line_content, level))
-                file.write("".join(arr_to_write))
+    with open(sys.argv[2], 'w', encoding='utf-8') as htmlfile:
+        for line in lines:
+            if line.startswith("#"):
+                level = 0
+                while line[level] == '#':
+                    level += 1
+                line_content = line[level:].strip()
+                htmlfile.write(f"<h{level}>{line_content}</h{level}>\n")
 
     sys.exit(0)
 
