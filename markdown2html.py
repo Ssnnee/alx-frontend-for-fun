7abd42f8986b1597e04d7bbc20458d866f8c52cc
Samuel Nandi <nandisamuelsne@gmail.com>
2024-07-21 23:32:52 +0100

    feat: parse md heading to genarate html

--- markdown2html.py
+++ markdown2html.py
@@ -8,6 +8,7 @@ from pathlib import Path
 
 def markdown2html():
     """Main function of the script."""
+    arr_to_write = []
 
     if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html",
@@ -18,8 +19,20 @@ def markdown2html():
         if not Path(sys.argv[i]).is_file():
             print("Missing {}".format(sys.argv[i]), file=sys.stderr)
             sys.exit(1)
-        else:
-            sys.exit(0)
+
+    with open(sys.argv[1], 'r') as mdfile:
+        content = mdfile.readlines()
+
+    for line in content:
+        parsed_line = line.split()
+        if (parsed_line[0].startswith("#")):
+            level = parsed_line[0].count("#")
+            line_content = line[level:-1].strip()
+            with open(sys.argv[2], mode="w", encoding="utf-8") as file:
+                arr_to_write.append("<h{}>{}<h{}>\n".format(
+                    level, line_content, level))
+
+                file.write("".join(arr_to_write))
 
 
 __name__ == "__main__" and markdown2html()
