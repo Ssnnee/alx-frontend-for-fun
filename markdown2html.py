df93cb0c15fde0af2f78660f550d00a324b45ce2
Samuel Nandi <nandisamuelsne@gmail.com>
2024-07-21 21:18:01 +0100

    feat: script usage

---
+++ markdown2html.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Convertes markdown files to html files."""
+
+
+import sys
+from pathlib import Path
+
+
+def markdown2html():
+    """Main function of the script."""
+
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html",
+              file=sys.stderr)
+        sys.exit(1)
+
+    for i in range(1, 3):
+        if not Path(sys.argv[i]).is_file():
+            print("Missing {}".format(sys.argv[i]), file=sys.stderr)
+            sys.exit(1)
+
+    sys.exit(0)
+
+
+__name__ == "__main__" and markdown2html()
